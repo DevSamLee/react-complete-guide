0d401d8d3c25269bdc086140931b4d029839649a
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2023-01-07 22:34:15 -0600

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 1, 6);
+    const expenseTitle = 'Dell Laptop';
+    const expenseAmount = 899.99;
+
     return (
         <div className="expense-item">
-            <div>January 6th 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Dell Laptop</h2>
-                <div className="expense-item__price">$899.99</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
