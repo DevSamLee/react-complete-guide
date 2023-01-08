64e81b6534857a5fe06728a97f73780c9b8ac30a
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2023-01-07 22:03:03 -0600

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <div>
+            <div>January 6th 2023</div>
+            <div>
+                <h2>Dell Laptop</h2>
+                <div>$899.99</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
