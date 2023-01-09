7d5ff15da70283e3cb612f3f51a0946b012f98eb
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2023-01-09 12:31:58 -0600

    Add ExpensesFilter.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -39,6 +39,10 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         };
         console.log(expenseData);
+        //to clear userInput
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate(new Date());
     };
 
     return (
@@ -46,7 +50,7 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
@@ -54,6 +58,7 @@ const ExpenseForm = () => {
                         type="number"
                         min="0.01"
                         step="0.01"
+                        value={enteredAmount}
                         onChange={amountChangeHandler}
                     />
                 </div>
@@ -63,6 +68,7 @@ const ExpenseForm = () => {
                         type="date"
                         min="2019-01-01"
                         max="2024-12-31"
+                        value={enteredDate}
                         onChange={dateChangeHandler}
                     />
                 </div>
