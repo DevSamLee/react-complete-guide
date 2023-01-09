676feced4d146599b918b5abecae0fd4e1ab549b
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2023-01-09 13:42:26 -0600

    Update Expenses.js
    Made lean return statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,24 +14,26 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+    let expensesContent = <p>No expense found.</p>;
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ));
+    }
+
     return (
         <Card className="expenses">
             <ExpensesFilter
                 selected={filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
-            // abusing
-            {filteredExpenses.length === 0 &&
-                <p>No expense found.</p>}
-            {filteredExpenses.length > 0 && 
-                filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
+            {expensesContent}
         </Card>
     );
 };
